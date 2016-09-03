eb636fff43053a9c7318663c167f47dfc01599fd
Océane Lecoq <oceane.lq@gmail.com>
2016-09-03 18:47:02 +0200

    YoloCommit

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
 
-	// Use this for initialization
+    public float speed;
+
 	void Start () {
 	    
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
-	}
+	    if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(speed, 0, 0));
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-speed, 0, 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, 0.5f, 0));
+        }
+    }
 }
