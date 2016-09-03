e479e3e4f4bb941d9ac1dbb440b43a12848a75a6
QuanticPotatoes <jdieuze@gmail.com>
2016-09-03 23:24:09 +0200

    Add mouse behavior

---
+++ Assets/Scripts/MouseBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseBehaviour : MonoBehaviour {
+
+    private Collider2D coll;
+    private bool isEncounter = false;
+    private Vector2 MousePosition;
+    Rigidbody2D Rigid;
+
+	// Use this for initialization
+	void Start () {
+        Rigid = GetComponent<Rigidbody2D>();
+        MousePosition = new Vector2(transform.position.x,transform.position.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isEncounter)
+        {
+            Rigid.AddForce(new Vector2(PlayerMouseController.Player.transform.position.x * MousePosition.x, PlayerMouseController.Player.transform.position.y / MousePosition.y));
+        }
+	}
+
+    
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "mouse" && !isEncounter)
+        {
+            PlayerMouseController.MouseNumber++;
+            Debug.Log(PlayerMouseController.MouseNumber++);
+            isEncounter = true;
+        }
+    }
+
+
+    void Desactive() {
+
+        RemoveMouse();
+
+    }
+
+
+    void RemoveMouse()
+    {
+        PlayerMouseController.MouseNumber--;
+    }
+
+}
---
+++ Assets/Scripts/PlayerMouseController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMouseController : MonoBehaviour {
+
+   
+    public static GameObject Player;
+    public static int MouseNumber;
+    public GameObject PlayerTemp;
+    // Use this for initialization
+    void Start () {
+        Player = PlayerTemp;
+    }
+	 
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
